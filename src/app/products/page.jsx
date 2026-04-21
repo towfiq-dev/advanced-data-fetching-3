b8560ba5d91ec121dda4736c10e6a453facfa67e
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-04-21 20:08:09 +0600

    Auto-update on Tue, Apr 21, 2026  8:08:09 PM

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,9 +1,26 @@
+import Product from '@/components/product/Product';
 import React from 'react';
-
-const productsPage = () => {
+const getProducts = async()=>{
+  try{
+    const res = await fetch('http://localhost:3004/products')
+  return res.json()
+  }catch(error){
+    throw new Error("Failed data fetch");
+  }
+}
+const productsPage = async() => {
+  const products = await getProducts()
   return (
-    <div>
-      Products
+    <div >
+      <h1>fffff{products.length}</h1>
+      <div className='grid grid-cols-3 justify-between gap-4 items-center mt-10 max-w-300 mx-auto'>
+        {
+        products.map((product, index)=> 
+        <Product product={product} 
+        key={index}>
+        </Product>)
+      }
+      </div>
     </div>
   );
 };
---
+++ src/components/product/Product.jsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Product = ({product}) => {
+  const {name, price, category, stock, rating, description, image, brand} = product
+  return (
+    <div className="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <Image
+    width={300}
+    height={300}
+      src={image}
+      alt={name} />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">{name}</h2>
+    <p>{description}</p>
+    <div className="card-actions justify-end">
+    <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+  );
+};
+
+export default Product;
